8a4406e625e0cb4c1b0a590263f79b277f7b7f5a
unknown <ahmedirheem2003@gmail.com>
2024-11-02 14:58:49 +0200

    Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        beVietnam: ['Be Vietnam Pro', 'sans-serif'],
+      },
+      colors: {
+        mainOrange: 'rgba(255, 149, 0, 1)',
+        lightOrange: 'rgba(255, 244, 229, 1)',
+        lightGray: 'rgba(241, 241, 243, 1)',
+      }
+    },
   },
   plugins: [],
 }
