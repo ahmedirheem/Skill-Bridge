b2ae2f972d831c6873df60aadd26620ca3de560f
unknown <ahmedirheem2003@gmail.com>
2024-11-09 21:04:00 +0200

    About Us Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         mainOrange: 'rgba(255, 149, 0, 1)',
         lightOrange: 'rgba(255, 244, 229, 1)',
         lightGray: 'rgba(241, 241, 243, 1)',
+      },
+      backgroundImage: {
+        aboutImage: 'url("../src/assets/images/Abstract Design.png")'
       }
     },
   },
